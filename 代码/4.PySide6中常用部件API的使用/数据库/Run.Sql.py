8306be83cfb6419251a78ee5bf0c0076547acb9a
H1DDENADM1N <65646535+H1DDENADM1N@users.noreply.github.com>
2024-03-21 16:51:50 +0800

    更新 数据库 查找

--- 代码/4.PySide6中常用部件API的使用/数据库/Run.Sql.py
+++ 代码/4.PySide6中常用部件API的使用/数据库/Run.Sql.py
@@ -1,18 +1,25 @@
 from Ui_Sql import Ui_Sql
 
 from PySide6.QtWidgets import ( QApplication, QWidget,
-                                QTableView, QHeaderView)
+                                QTableView, QHeaderView, QCompleter)
+
 from PySide6.QtGui import QAction, QDesktopServices
+
 from PySide6.QtCore import (    Qt, QUrl,
-                                QSortFilterProxyModel)
+                                QSortFilterProxyModel, QStringListModel)
+
 from PySide6.QtSql import ( QSqlDatabase,  # 连接数据库
                             QSqlQueryModel,  # 只读
                             QSqlTableModel)  # 可读写
+
+import re
 from pypinyin import lazy_pinyin
+
 import os
 
 # 切换到当前文件的目录
 os.chdir(os.path.dirname(__file__))
+
 
 # 自定义代理模型，按拼音排序，数据一多非常卡
 class PinyinSortFilterProxyModel(QSortFilterProxyModel):
@@ -70,6 +77,7 @@ class MyWindow(QWidget):
         self.model.select()
 
         self.setup_proxy_model()
+        self.setup_string_list_model()
 
     # 创建代理模型，保持源模型的纯净和可重用性
     def setup_proxy_model(self):
@@ -87,9 +95,17 @@ class MyWindow(QWidget):
         # 设置 代理模型 表头
         # for i, letter in enumerate(['序号 index', '姓名 name', '地址 address', '邮箱 email']):
         
-        for i, letter in enumerate(['序号 index', '姓名 name', '地址 address', '邮箱 email', '姓名拼音']):
+        for i, letter in enumerate(['0 序号 index', '1 姓名 name', '2 地址 address', '3 邮箱 email', '4 姓名拼音']):
             self.sort_filter_proxy_model.setHeaderData(i, Qt.Horizontal, letter)
 
+    # 创建字符串列表模型
+    def setup_string_list_model(self):
+        self.string_list_model = QStringListModel()
+        # 设置补全器
+        self.completer = QCompleter(self.string_list_model, self)
+        self.completer.setCaseSensitivity(Qt.CaseInsensitive)  # 设置不区分大小写
+        # 将 QCompleter 设置到 QLineEdit
+        self.ui.lineEdit_data_filter.setCompleter(self.completer)
 
 
     '''
@@ -111,8 +127,11 @@ class MyWindow(QWidget):
         self.ui.tableView.horizontalHeader().setSectionResizeMode(QHeaderView.Interactive)
         # 允许排序
         self.ui.tableView.setSortingEnabled(True)
+        # 单元格合并
+        # self.ui.tableView.setSpan(0, 0, 3, 2)
         # 设置默认按姓名拼音列排序
         self.sort_filter_proxy_model.sort(4)
+
 
 
     '''
@@ -123,7 +142,47 @@ class MyWindow(QWidget):
 
     # 连接信号/槽
     def setup_signals(self):
-        pass
+        self.ui.lineEdit_data_filter.textChanged.connect(self.use_filter)
+        self.ui.lineEdit_filter_range.textChanged.connect(self.set_filter_range)
+
+    def use_filter(self):
+        text = self.ui.lineEdit_data_filter.text()
+        try:
+            # 尝试编译正则表达式来检查语法
+            re.compile(text)
+            # 如果编译成功，设置代理模型的过滤规则，使用 正则表达式 进行搜索 过滤
+            self.sort_filter_proxy_model.setFilterRegularExpression(text)
+            self.ui.label_data_filter.setText("数据过滤")  # 清除错误信息
+            # 添加过滤结果到字符串列表模型补全器
+            text_list = []
+            # 遍历代理模型的所有行和列
+            for row in range(self.sort_filter_proxy_model.rowCount()):
+                for column in range(self.sort_filter_proxy_model.columnCount()):
+                    # 获取模型索引
+                    index = self.sort_filter_proxy_model.index(row, column)
+                    # 获取单元格数据
+                    data = self.sort_filter_proxy_model.data(index)
+                    # 将数据添加到文本列表
+                    text_list.append(data)
+
+            self.string_list_model.setStringList(text_list)
+        except re.error as e:
+            # 如果编译失败，显示错误信息
+            self.ui.label_data_filter.setText(f"正则表达式错误: {e}")
+
+    def set_filter_range(self):
+        # 获取需要过滤的列
+        text = self.ui.lineEdit_filter_range.text()
+        list = text.split(",")
+        # 设置过滤规则
+        for col in list:
+            try:
+                col = int(col.strip())
+                self.sort_filter_proxy_model.setFilterKeyColumn(col)
+            except ValueError:
+                self.ui.label_filter_range.setText("过滤适用范围（列）设置内容格式错误")
+                return
+        self.ui.label_filter_range.setText("过滤适用范围（列）")
 
     # 设置上下文菜单
     def setup_context_menu(self):
--- 代码/4.PySide6中常用部件API的使用/数据库/Ui_Sql.py
+++ 代码/4.PySide6中常用部件API的使用/数据库/Ui_Sql.py
@@ -15,8 +15,9 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QHeaderView, QLabel, QSizePolicy,
-    QTableView, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QHBoxLayout, QHeaderView, QLabel,
+    QLineEdit, QSizePolicy, QTableView, QVBoxLayout,
+    QWidget)
 
 class Ui_Sql(object):
     def setupUi(self, Sql):
@@ -25,6 +26,36 @@ class Ui_Sql(object):
         Sql.resize(575, 796)
         self.verticalLayout = QVBoxLayout(Sql)
         self.verticalLayout.setObjectName(u"verticalLayout")
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.label_data_filter = QLabel(Sql)
+        self.label_data_filter.setObjectName(u"label_data_filter")
+
+        self.horizontalLayout.addWidget(self.label_data_filter)
+
+        self.lineEdit_data_filter = QLineEdit(Sql)
+        self.lineEdit_data_filter.setObjectName(u"lineEdit_data_filter")
+
+        self.horizontalLayout.addWidget(self.lineEdit_data_filter)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.label_filter_range = QLabel(Sql)
+        self.label_filter_range.setObjectName(u"label_filter_range")
+
+        self.horizontalLayout_2.addWidget(self.label_filter_range)
+
+        self.lineEdit_filter_range = QLineEdit(Sql)
+        self.lineEdit_filter_range.setObjectName(u"lineEdit_filter_range")
+
+        self.horizontalLayout_2.addWidget(self.lineEdit_filter_range)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+
         self.label = QLabel(Sql)
         self.label.setObjectName(u"label")
 
@@ -43,6 +74,10 @@ class Ui_Sql(object):
 
     def retranslateUi(self, Sql):
         Sql.setWindowTitle(QCoreApplication.translate("Sql", u"\u6570\u636e\u5e93", None))
+        self.label_data_filter.setText(QCoreApplication.translate("Sql", u"\u6570\u636e\u8fc7\u6ee4", None))
+        self.lineEdit_data_filter.setText(QCoreApplication.translate("Sql", u"/*", None))
+        self.label_filter_range.setText(QCoreApplication.translate("Sql", u"\u8fc7\u6ee4\u9002\u7528\u8303\u56f4\uff08\u5217\uff09", None))
+        self.lineEdit_filter_range.setText(QCoreApplication.translate("Sql", u"1,2", None))
         self.label.setText(QCoreApplication.translate("Sql", u"\u6570\u636e\u5e93", None))
     # retranslateUi
 
